221eacb911b208fc578d20c9fe99cc5ba0edd8b8
LOBO50K <playperfect344@gmail.com>
2024-08-19 16:43:48 -0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,14 +3,14 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     let don = `
 - ᴀᴘᴏʏᴀ ᴀᴍɪ ᴄʀᴇᴀᴅᴏʀ :3
 ᴀᴠᴇᴄᴇꜱ ʟᴏꜱ ꜱᴇʀᴠɪᴅᴏʀᴇꜱ ɴᴏꜱ ᴄᴜᴇꜱᴛᴀɴ :c
-- 𝙋𝘼𝙔𝙋𝘼𝙇
-• *Link :* https://paypal.me/JoseCuri123
-- 𝙔𝘼𝙋𝙀
-• *Numero :* 913899071
+- PAYPAL
+• *Link :* https://www.paypal.me/BenjaBarreto
+- MERCADO PAGO
+• *cvu :* 0000003100078317676333
 ᴘᴏʀ ꜱᴜ ᴀᴘᴏʏᴏ ʟᴇꜱ ᴏᴛᴏʀɢᴀʀᴇᴍᴏꜱ ᴘʀᴇᴍɪᴜᴍ
 ʏ ᴜɴ ᴀᴄᴄᴇꜱᴏ ᴀʟ ʙᴏᴛ ᴀ ᴄᴜᴀʟQᴜɪᴇʀ ɢʀᴜᴘᴏ.
 `
-let img = 'https://telegra.ph/file/290082cebfe4bb0d435a1.jpg'
+let img = 'https://telegra.ph/file/99f6a3fc5f94dca595380.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m, null,)
 //conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
 }
